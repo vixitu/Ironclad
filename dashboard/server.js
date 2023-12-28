99c0907e4c63788d096c75c54df0ab39e7728ce8
vixitu <kasbevers@gmail.com>
2023-12-28 16:19:59 +0100

    web home done

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,6 +1,17 @@
 const express = require('express')
+
 const app = express()
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'pug')
 
+
+
+app.use(express.static(`${__dirname}/assets`));
+app.locals.basedir = `${__dirname}/assets`;
+
+app.get('/', (req, res) => res.render('index', {subtitle: 'Home'}))
+app.get('/commands', (req, res) => res.render('commands'))
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server is live on port ${port}`))
+
